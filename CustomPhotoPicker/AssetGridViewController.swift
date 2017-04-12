e84873186f0356a3ab56d63f3b47d75228dce180
ThangTruong <thangtm6@fsoft.com.vn>
2017-04-12 14:23:29 +0700

    display Image in collectionview

---
+++ CustomPhotoPicker/AssetGridViewController.swift
@@ -0,0 +1,165 @@
+//
+//  CollectionViewController.swift
+//  CustomPhotoPicker
+//
+//  Created by Trương Thắng on 4/12/17.
+//  Copyright © 2017 Trương Thắng. All rights reserved.
+//
+
+import UIKit
+import Photos
+import PhotosUI
+
+class AssetGridViewController: UICollectionViewController {
+
+    var fetchResult: PHFetchResult<PHAsset>!
+    var assetCollection: PHAssetCollection!
+    
+    
+    fileprivate let imageManager = PHCachingImageManager()
+    private var _thumbnailSize: CGSize?
+    
+    var thumbnailSize: CGSize {
+        set {
+            _thumbnailSize = newValue
+        }
+        get {
+            if _thumbnailSize == nil {
+                updateThumbnailSize()
+            }
+            return _thumbnailSize ?? []
+        }
+    }
+    
+    func updateThumbnailSize() {
+        let numberOfItemInRow : CGFloat = 4.0
+        if let bounds = AppDelegate.shared.window?.bounds {
+            
+            let size = bounds.width / numberOfItemInRow
+            let scale = UIScreen.main.scale
+            
+            return CGSize(width: size * scale, height: size * scale)
+        }
+        return CGSize.zero
+    }
+    fileprivate var thumbnailSize: CGSize = {
+        
+    }()
+    
+    fileprivate var previousPreheatRect = CGRect.zero
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        PHPhotoLibrary.shared().register(self)
+        
+        // If we get here without a segue, it's because we're visible at app launch,
+        // so match the behavior of segue from the default "All Photos" view.
+        if fetchResult == nil {
+            let allPhotosOptions = PHFetchOptions()
+            allPhotosOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
+            fetchResult = PHAsset.fetchAssets(with: allPhotosOptions)
+        }
+    }
+    
+    deinit {
+        PHPhotoLibrary.shared().unregisterChangeObserver(self)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Determine the size of the thumbnails to request from the PHCachingImageManager
+        let scale = UIScreen.main.scale
+        let cellSize = (collectionViewLayout as! UICollectionViewFlowLayout).itemSize
+        thumbnailSize = CGSize(width: cellSize.width * scale, height: cellSize.height * scale)
+        
+    }
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return fetchResult.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let asset = fetchResult.object(at: indexPath.item)
+        
+        // Dequeue a GridViewCell.
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: GridViewCell.self), for: indexPath) as? GridViewCell
+            else { fatalError("unexpected cell in collection view") }
+        
+        // Add a badge to the cell if the PHAsset represents a Live Photo.
+        if asset.mediaSubtypes.contains(.photoLive) {
+            cell.livePhotoBadgeImage = PHLivePhotoView.livePhotoBadgeImage(options: .overContent)
+        }
+        
+        // Request an image for the asset from the PHCachingImageManager.
+        cell.representedAssetIdentifier = asset.localIdentifier
+        imageManager.requestImage(for: asset, targetSize: thumbnailSize, contentMode: .aspectFill, options: nil, resultHandler: { image, _ in
+            // The cell may have been recycled by the time this handler gets called;
+            // set the cell's thumbnail image only if it's still showing the same asset.
+            if cell.representedAssetIdentifier == asset.localIdentifier {
+                cell.thumbnailImage = image
+            }
+        })
+        
+        return cell
+        
+    }
+}
+
+// MARK: PHPhotoLibraryChangeObserver
+extension AssetGridViewController: PHPhotoLibraryChangeObserver {
+    func photoLibraryDidChange(_ changeInstance: PHChange) {
+        
+        guard let changes = changeInstance.changeDetails(for: fetchResult)
+            else { return }
+        
+        // Change notifications may be made on a background queue. Re-dispatch to the
+        // main queue before acting on the change as we'll be updating the UI.
+        DispatchQueue.main.sync {
+            // Hang on to the new fetch result.
+            fetchResult = changes.fetchResultAfterChanges
+            if changes.hasIncrementalChanges {
+                // If we have incremental diffs, animate them in the collection view.
+                guard let collectionView = self.collectionView else { fatalError() }
+                collectionView.performBatchUpdates({
+                    // For indexes to make sense, updates must be in this order:
+                    // delete, insert, reload, move
+                    if let removed = changes.removedIndexes, removed.count > 0 {
+                        collectionView.deleteItems(at: removed.map({ IndexPath(item: $0, section: 0) }))
+                    }
+                    if let inserted = changes.insertedIndexes, inserted.count > 0 {
+                        collectionView.insertItems(at: inserted.map({ IndexPath(item: $0, section: 0) }))
+                    }
+                    if let changed = changes.changedIndexes, changed.count > 0 {
+                        collectionView.reloadItems(at: changed.map({ IndexPath(item: $0, section: 0) }))
+                    }
+                    changes.enumerateMoves { fromIndex, toIndex in
+                        collectionView.moveItem(at: IndexPath(item: fromIndex, section: 0),
+                                                to: IndexPath(item: toIndex, section: 0))
+                    }
+                })
+            } else {
+                // Reload the collection view if incremental diffs are not available.
+                collectionView!.reloadData()
+            }
+        }
+    }
+}
---
+++ CustomPhotoPicker/GridViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  GridViewCell.swift
+//  CustomPhotoPicker
+//
+//  Created by Trương Thắng on 4/12/17.
+//  Copyright © 2017 Trương Thắng. All rights reserved.
+//
+
+import UIKit
+
+class GridViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var livePhotoBadgeImageView: UIImageView!
+    
+    var representedAssetIdentifier: String!
+    
+    var thumbnailImage: UIImage! {
+        didSet {
+            imageView.image = thumbnailImage
+        }
+    }
+    var livePhotoBadgeImage: UIImage! {
+        didSet {
+            livePhotoBadgeImageView.image = livePhotoBadgeImage
+        }
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        imageView.image = nil
+        livePhotoBadgeImageView.image = nil
+    }
+}
